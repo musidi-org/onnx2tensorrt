9cf0f19a72a4ba0483f184bb0fb054cd0746e7e7
Meng Lin <menglinmaker@gmail.com>
2024-05-24 16:29:22 +1000

    Feature: Use cmd line trtexec to convert and test model

--- modal/util.py
+++ modal/util.py
@@ -1,19 +1,18 @@
 import os
 import time
-import onnx
+from dotenv import load_dotenv
+load_dotenv()
+
+from bucketFile import downloadBucketFile, uploadBucketFile
 
 def createFile(filePath):
   if not os.path.exists(filePath): 
     with open(filePath, 'w') as file: 
       file.write('')
 
-
-
 def fileBaseName(filePath):
   fileName = os.path.basename(filePath)
   return os.path.splitext(fileName)[0]
-
-
 
 class TimeStamp:
   def __init__(self) -> None:
@@ -29,46 +28,24 @@ class TimeStamp:
     print(f'\n\n\n{log}:', timeGap, '\n\n\n')
     return timeGap
 
-
-
-def tuneModel(modelPath):
-  import tensorrt as trt
+def tuneModel(modelKey):
+  modelPath = 'test.onnx'
+  enginePath = f'{modelPath}.trt'
   timeStamp = TimeStamp()
   
-  trtLogger = trt.Logger(trt.Logger.INFO)
-  trtBuilder = trt.Builder(trtLogger)
-  trtNetwork = trtBuilder.create_network(0)
-  onnxParser = trt.OnnxParser(trtNetwork, trtLogger)
-  timeStamp.stampPrint(f'SETUP:')
-  
-  parsedSuccess = onnxParser.parse_from_file(modelPath)
-  for idx in range(onnxParser.num_errors):
-    print(onnxParser.get_error(idx))
-  timeStamp.stampPrint(f'PARSED: {parsedSuccess}')
-  
-  if parsedSuccess:
-    trtConfig = trtBuilder.create_builder_config()
-    serializedEngine = trtBuilder.build_serialized_network(trtNetwork, trtConfig)
-    timeStamp.stampPrint(f'CREATE SERIALIZE ENGINE:')
+  downloadBucketFile(modelPath, os.environ['ONNX_BUCKET'], modelKey)
+  timeStamp.stampPrint(f'DOWNLOAD ONNX MODEL:')
 
-    enginePath = 'sample.engine'
-    with open(enginePath, 'wb') as f:
-      f.write(serializedEngine)
-      timeStamp.stampPrint(f'SAVE SERIALIZE ENGINE:')
-      
-    model = onnx.load("path/to/model.onnx")
-    inputShapes = [[d.dim_value for d in _input.type.tensor_type.shape.dim] for _input in model.graph.input]
-    stringShape = 'x'.join(inputShapes[0])
-    validateResult = os.system(f'trtexec --shapes=input:{stringShape} --loadEngine={enginePath}')
-    if validateResult != 0:
-      print('VALIDATION FAILED')
-    timeStamp.stampPrint(f'VALIDATE ENGINE:')
-
-    with open(enginePath, 'rb') as f:
-      serializedEngine = f.read()
-      timeStamp.stampPrint(f'LOAD SERIALIZE ENGINE:')
-    
-    runtime = trt.Runtime(trtLogger)
-    engine = runtime.deserialize_cuda_engine(serializedEngine)
-    context = engine.create_execution_context()
-    # context.set_tensor_address(name, ptr)
+  conversionResult = os.system(f'trtexec --onnx={modelPath} --fp16 --saveEngine={enginePath}')
+  if conversionResult != 0:
+    print('CONVERSION FAILED')
+  timeStamp.stampPrint(f'CREATE TENSORRT ENGINE:')
+  
+  validateResult = os.system(f'trtexec --loadEngine={enginePath}')
+  if validateResult != 0:
+    print('VALIDATION FAILED')
+  timeStamp.stampPrint(f'VALIDATE ENGINE:')
+  
+  engineKey = f'{modelKey}.trt'
+  uploadBucketFile(enginePath, os.environ['TRT_BUCKET'], engineKey)
+  timeStamp.stampPrint(f'UPLOAD TRT ENGINE:')
